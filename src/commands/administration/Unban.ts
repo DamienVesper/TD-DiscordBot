ea9d19bcb3fe521975a32b0963bfa10ef8a3b8f0
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-22 19:42:13 +0530

    Update Unban.ts

--- src/commands/administration/Unban.ts
+++ src/commands/administration/Unban.ts
@@ -53,6 +53,8 @@ export default class Ban extends Command {
         if (banTimers[`${userFound.username}`]) delete banTimers[`${userFound.username}`];
         await fs.writeFileSync(`./data/banTimers.json`, JSON.stringify(banTimers, null, 4));
 
+        if (!userFound.isSuspended) return message.reply(`:x: That user is not banned.`);
+
         message.reply(`:white_check_mark: **${userFound.username}** has been unbanned!`);
 
         userFound.isSuspended = false;
