e99c6e4f6436424c449faba30bf43933bfff7559
sev-ofc <47048628+sev-ofc@users.noreply.github.com>
2021-03-21 16:34:11 -0700

    Added 2 new commands

---
+++ src/commands/administration/FetchEmail.ts
@@ -0,0 +1,63 @@
+// Import first-party classes
+import Command from "../../modules/commandapi/Command";
+import { CommandCategory } from "../../modules/commandapi/CommandCategory";
+import Console from "../../modules/commandapi/interpreter/Console";
+import ICommandField, { CommandField } from "../../modules/commandapi/ICommandField";
+import Main from "../../Main";
+// Import core Node modules and dependencies
+import Discord, { TextChannel, Message, Guild } from "discord.js";
+import fs from 'fs';
+
+import mongodb from 'mongodb';
+import axios from 'axios';
+
+import mongoose from 'mongoose';
+import User from '../../modules/Models/User';
+import Sticker from '../../modules/Models/Sticker';
+import { userInfo } from "os";
+
+export default class FetchEmail extends Command {
+    // Define the fields for the command
+    private static commandFields = new CommandField(
+        `fetchemail`, // NAME
+        `Gets user email`, // DESCRIPTION
+        `fetchemail [stickername]`, // USAGE - [] = MANDATORY () = OPTIONAL
+        [`fetchemail sev`], // EXAMPLES
+        CommandCategory.ADMINISTRATION, // CATEGORY
+        1, // MIN ARGS
+        1, // MAX ARGS
+        [`ADMINISTRATOR`], // REQUIRED PERMS
+        false, // BOT OWNER ONLY
+        false, // TRUSTED ONLY
+        [], // BLACKLISTED USERS
+        [], // WHITELISTED GUILDS
+        false, // DELETE ON FINISH
+        true, // SIMULATE TYPING
+        500, // SPAM TIMEOUT
+        ["getemail"] // ALIASES
+    );
+
+    /**
+     * Constructs a new instance of the "Test"
+     * command class
+     * @param cmdConsole The interpreter's console instance
+     */
+    constructor (cmdConsole:Console) {
+        // Call the superclass with the command fields
+        super(FetchEmail.commandFields, cmdConsole);
+    }
+
+    public async run (bot:Main, message:Discord.Message, args:string[], calledName:string):Promise<any> {
+        // Assert the argument count
+		super.assertArgCount(args.length, message);
+		
+        if (message.channel.id !== `794326024858763304`) return message.reply(`:x: This command can only be used in <#794326024858763304>`);
+
+        const userFound: any = await User.findOne({ username: args[0] });
+		if (!userFound) return message.channel.send(`:x: The username you provided was invalid!`);
+		
+		let email = userFound.email
+		message.reply(":white_check_mark: The email address for user **" + userFound.username + `** is ||${userFound.email}||`);
+
+}
+}
